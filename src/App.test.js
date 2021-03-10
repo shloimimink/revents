660cb6d2680a30298d69331c3ee0fdab81de1ee9
shloimiminkowicz <shloimimink@gmail.com>
2021-03-10 13:46:58 -0600

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './app/layout/App';
 
 test('renders learn react link', () => {
   render(<App />);

5dccd2e47d3253ecc3c0e3fd1d8bf5e1cf99c0a0
shloimiminkowicz <shloimimink@gmail.com>
2021-03-11 19:47:36 -0600

    End of section 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './app/layout/Styles.css'
 import App from './app/layout/App';
 
 const rootEl = document.getElementById('root')
 
 function render() {
-   ReactDOM.render(<App />, rootEl)
+    ReactDOM.render(<App/>, rootEl)
 }
 
 if (module.hot) {
